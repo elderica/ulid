7fb36e3921945121b8057df47f11315dc666c7ab
elderica <1130138+elderica@users.noreply.github.com>
2023-08-15 19:03:21 +0900

    Remove duplicate tests

--- tests/test-ulid.lisp
+++ tests/test-ulid.lisp
@@ -38,33 +38,6 @@
   (is (equalp (ulid::encode-time 1470264322240 12)
 	      "0001AS99AA60")))
 
-(test test-bytes-to-integer-2
-  (is (equal
-       (ulid::bytes-to-integer
-	(make-array 4
-		    :element-type '(unsigned-byte 8)
-		    :initial-contents '(#xDE #xAD #xBE #xEF)))
-       #xDEADBEEF)))
-
-(test test-integer-to-bytes-1
-  (is (equalp (ulid::integer-to-bytes #xDEADBEEF 4)
-	      (vector #xDE #xAD #xBE #xEF))))
-
-(test test-integer-to-bytes-2
-  (is (equalp (ulid::integer-to-bytes #xDEADBEEF 8)
-	      (vector #x00 #x00 #x00 #x00 #xDE #xAD #xBE #xEF))))
-
-(def-suite encode-time)
-(in-suite encode-time)
-
-(test should-return-expected-encoded-result
-  (is (equalp (ulid::encode-time 1469918176385 10)
-	      "01ARYZ6S41")))
-
-(test should-change-length-property
-  (is (equalp (ulid::encode-time 1470264322240 12)
-	      "0001AS99AA60")))
-
 (test should-truncate-time-if-not-enough-length
   (is (equalp (ulid::encode-time 1470118279201 8)
 	      "AS4Y1E11")))
