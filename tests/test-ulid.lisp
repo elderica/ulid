76eae09311ffd8c4ee930684ac7c54fb14f82d6e
elderica <1130138+elderica@users.noreply.github.com>
2023-08-20 18:25:23 +0900

    fix: reorder arguments in test

--- tests/test-ulid.lisp
+++ tests/test-ulid.lisp
@@ -3,37 +3,38 @@
 (def-suite* encode-decode-base32)
 
 (test should-return-expected-encoded-result
-  (is (string-equal (ulid::u128->base32 1469918176385)
-		    "01ARYZ6S41"
-		    :start1 16)))
+  (is (string-equal
+       "01ARYZ6S41"
+       (ulid::u128->base32 1469918176385)
+       :start2 16)))
 
 (def-suite* ulid)
 
 (test should-return-correct-length
-  (is (eql (length (ulid:ulid->base32 (ulid:generate-now)))
-	   26)))
+  (is (= 26
+	 (length (ulid:ulid->base32 (ulid:generate-now))))))
 
 (def-suite* u128-ulid)
 
 (test reversible-conversion
-  (is (= (ulid->u128
-	  (u128->ulid 1469918176385))
-	 1469918176385)))
+  (is (= 1469918176385
+	 (ulid->u128
+	  (u128->ulid 1469918176385)))))
 
 (def-suite* base32-ulid)
 
 (test should-return-expected-base32-encoded-result
-  (is (string-equal (ulid->base32
-		     (u128->ulid #x41414141414141414141414141414141))
-		    "21850M2GA1850M2GA1850M2GA1")))
+  (is (string-equal "21850M2GA1850M2GA1850M2GA1"
+		    (ulid->base32
+		     (u128->ulid #x41414141414141414141414141414141)))))
 
 (test should-return-decoded-integer
-  (is (= (base32->u128 "21850M2GA1850M2GA1850M2GA1")
-	 #x41414141414141414141414141414141)))
+  (is (= #x41414141414141414141414141414141)
+      (base32->u128 "21850M2GA1850M2GA1850M2GA1")))
 
 (test should-return-decoded-ulid
-  (is (equalp (base32->ulid "21850M2GA1850M2GA1850M2GA1")
-	      (u128->ulid #x41414141414141414141414141414141))))
+  (is (equalp (u128->ulid #x41414141414141414141414141414141)
+	      (base32->ulid "21850M2GA1850M2GA1850M2GA1"))))
 
 (test should-throw-error-when-out-of-range-value
   (signals ulid-out-of-range
